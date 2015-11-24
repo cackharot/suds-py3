6a72fd7794e80823c8ab0d2165e6e1e2e98dd494
cackharot <cackharot@gmail.com>
2015-11-24 19:28:52 +0530

    Updated client and java soap service to use basic authentication

--- examples/java-soap/src/org/cackharot/services/publisher/HelloServicePublisher.java
+++ examples/java-soap/src/org/cackharot/services/publisher/HelloServicePublisher.java
@@ -1,14 +1,43 @@
 package org.cackharot.services.publisher;
 
+import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
 import org.cackharot.services.HelloService;
 
+import javax.xml.ws.Binding;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloServicePublisher {
-    public static void main(String[] args) {
-      final String url = "http://localhost:8181/soap/helloservice";
-      System.out.println(String.format("Starting the service at %s", url));
-      System.out.println(String.format("See WSDL at %s?wsdl", url));
-      Endpoint.publish(url, new HelloService());
+    public static void main(String[] args) throws IOException {
+        Endpoint endpoint = Endpoint.create(new HelloService());
+
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8181), 0);
+        server.start();
+
+        HttpContext context = server.createContext("/soap/helloservice");
+
+        context.setAuthenticator(new BasicAuthenticator("test") {
+            @Override
+            public boolean checkCredentials(String username, String pass) {
+                return "bob".equals(username) && "catbob".equals(pass);
+            }
+        });
+
+        printInfo("http:/" + server.getAddress().toString() + context.getPath());
+
+        endpoint.publish(context);
     }
+
+    private static void printInfo(String url) {
+        System.out.println(String.format("Starting the service at %s", url));
+        System.out.println(String.format("See WSDL at %s?wsdl", url));
+    }
+
 }
--- examples/test_client.py
+++ examples/test_client.py
@@ -3,14 +3,20 @@ import os
 suds_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(suds_path)
 
-if __name__ == '__main__':
-    from suds.client import Client
-    url = 'http://localhost:8181/soap/helloservice?wsdl'
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
+def set_log():
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('suds.client').setLevel(logging.DEBUG)
+    logging.getLogger('suds.transport').setLevel(logging.DEBUG)
 
-    client = Client(url)
-    # print(client)
+
+def call_service(url):
+    from suds.client import Client
+
+    # set_log()
+
+    client = Client(url, username='bob', password='catbob')
+
     print("Calling: sayHello()")
     result = client.service.sayHello('Username')
     print("Result: %s" % result)
@@ -19,3 +25,10 @@ if __name__ == '__main__':
     print("Calling: add()")
     sum = client.service.add(a, b)
     print("Result: Sum of %f + %f = %f" % (a,b,sum))
+
+if __name__ == '__main__':
+    url = 'http://localhost:8181/soap/helloservice?wsdl'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+
+    call_service(url)
