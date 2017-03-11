91e8aca217b81433eee5b7f7a8616b31fc6cd2a2
cackharot <cackharot@gmail.com>
2017-03-11 12:58:52 -0500

    bump up version to 1.3.3.0

---
+++ publish.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 setup.py sdist upload
--- suds/__init__.py
+++ suds/__init__.py
@@ -25,8 +25,8 @@ from .compat import basestring, unicode
 # Project properties
 #
 
-__version__ = '1.3.2.0'
-__build__ = "IN 20160428"
+__version__ = '1.3.3.0'
+__build__ = "IN 20170311"
 
 #
 # Exceptions
