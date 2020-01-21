83a0f1df6ffc745d48f16856f423d0e9035425fb
Tadeusz Magura-Witkowski <Teeed@users.noreply.github.com>
2020-01-21 16:17:55 +0100

    Fixed automatic initialization of cache

--- suds/client.py
+++ suds/client.py
@@ -104,7 +104,8 @@ class Client(object):
         options = Options()
         options.transport = HttpAuthenticated()
         self.options = options
-        options.cache = ObjectCache(days=1)
+        if 'cache' not in kwargs:
+            options.cache = ObjectCache(days=1)
         self.set_options(**kwargs)
         reader = DefinitionsReader(options, Definitions)
         self.wsdl = reader.open(url)
