21c1f108b72e1ce93cda62a232ec02ac03ceed84
Thiruvarasan S <thirus@thoughtworks.com>
2020-03-05 14:21:58 -0500

    Add unit test for '&' encoding special chars

--- tests/encode_specials_test.py
+++ tests/encode_specials_test.py
@@ -6,5 +6,4 @@ class TestEncodeSpecials(unittest.TestCase):
     def test_specials_chars(self):
         encoder = suds.sax.Encoder()
         assert encoder.needsEncoding('&lt;test') == True # output is True
-        print(encoder.encode('&lt;test'))
         assert encoder.encode('&lt;test') == '&amp;lt;test'
