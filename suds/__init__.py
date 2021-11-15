871d0c363607c6bee06236e743e2804f513ba22a
Cackharot <cackharot@gmail.com>
2021-11-15 22:54:44 +0530

    bump up to 1.4.5.0

--- suds/__init__.py
+++ suds/__init__.py
@@ -25,8 +25,8 @@ from .compat import basestring, unicode
 # Project properties
 #
 
-__version__ = '1.4.4.1'
-__build__ = "IN 20210108"
+__version__ = '1.4.5.0'
+__build__ = "IN 20211115"
 
 #
 # Exceptions
